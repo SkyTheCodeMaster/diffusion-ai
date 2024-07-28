f839cfc6de8fa3dc848d823f5b72059ae5ba8058
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-07-27 20:23:25 -0400

    Make diffusion work

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 from aiohttp import web
 from aiohttp.web import Response
-
+from utils.diffusion import generate
 from utils.cors import add_cors_routes
 from utils.limiter import Limiter
 
@@ -34,6 +34,27 @@ async def get_lp_get(request: Request) -> Response:
     packet["db_size"] = database_size_record.get("pg_size_pretty","-1 kB")
 
   return web.json_response(packet)
+
+@routes.post("/diffusion/")
+@limiter.limit("10/m")
+async def post_diffusion(request: Request) -> Response:
+  body = await request.json()
+
+  if "prompt" not in body:
+    return Response(status=400,text="must include prompt in body")
+
+  prompt = body.get("prompt")
+  negative_prompt = body.get("negative_prompt", None)
+  width = body.get("width", None)
+  height = body.get("height", None)
+
+  try:
+    result = await generate(prompt, negative_prompt=negative_prompt, width=width, height=height)
+  except Exception as e:
+    return Response(status=500, text=str(e))
+  
+  return web.json_response(result)
+
 
 async def setup(app: web.Application) -> None:
   for route in routes:
---
+++ src/utils/diffusion.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+import asyncio
+import base64
+import gc
+import logging
+import time
+import tomllib
+from io import BytesIO
+from typing import TYPE_CHECKING
+
+import torch
+from diffusers import DiffusionPipeline
+
+if TYPE_CHECKING:
+  from PIL import Image
+
+with open("config.toml") as f:
+  config = tomllib.loads(f.read())
+
+LOG = logging.getLogger(__name__)
+MODEL_ID = config["ai"]["model"]
+DEVICE = config["ai"]["device"]
+MODEL_LOCK = asyncio.Lock()
+pipe = DiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
+pipe.to(DEVICE)
+
+if not torch.cuda.is_available() and "cuda" in DEVICE:
+  raise Exception("Cuda is not available for model inference!")
+
+
+def generate_images(
+  prompt: str,
+  *,
+  negative_prompt: str = None,
+  width: int = None,
+  height: int = None,
+) -> list[Image.Image]:
+  output = pipe(
+    prompt, negative_prompt=negative_prompt, width=width, height=height
+  )
+  images: list[Image.Image] = output.images
+  return images
+
+
+def images_to_base64(images: list[Image.Image]) -> list[str]:
+  output = []
+
+  for image in images:
+    b = BytesIO()
+    image.save(b, format="png")
+    output.append(base64.b64encode(b.read()).decode())
+
+  return output
+
+
+async def generate(
+  prompt: str,
+  *,
+  negative_prompt: str = None,
+  width: int = None,
+  height: int = None,
+) -> list[str]:
+  "Take in prompt and some parameters, return list of base64-encoded PNGs."
+  loop = asyncio.get_running_loop()
+  start = time.time()
+
+  def t():
+    return round(time.time() - start, 4)
+
+  if width is not None and width % 8 != 0:
+    raise ValueError("Width must be a multiple of 8!")
+  if height is not None and height % 8 != 0:
+    raise ValueError("Height must be a multiple of 8!")
+
+  LOG.info(f"[{t()}] Waiting for lock...")
+  async with MODEL_LOCK:
+    LOG.info(f"[{t()}] Lock acquired. Generating images...")
+    images = await loop.run_in_executor(
+      None,
+      lambda: generate_images(
+        prompt, negative_prompt=negative_prompt, width=width, height=height
+      ),
+    )
+    LOG.info(f"[{t()}] Images generated, transcribing to base64...")
+    b64 = await loop.run_in_executor(None, images_to_base64, images)
+    LOG.info(f"[{t()}] Base64 acquired. Finishing up...")
+  cleanup()
+  LOG.info(f"[{t()}] All cleaned up!")
+  return b64
+
+
+def cleanup():
+  torch.cuda.empty_cache()
+  torch.cuda.ipc_collect()
+  gc.collect()
